c088e1331877f2904fb3b3b962587110981f7df5
paulvstrashnov <paulvstrashnov@mail.ru>
2022-05-06 01:12:35 +0300

    Added build workflow

--- src/action.js
+++ src/action.js
@@ -1,5 +1,39 @@
-async function run() {
+const core = require('@actions/core');
+const EasyYandexS3 = require("easy-yandex-s3"); // https://github.com/powerdot/easy-yandex-s3
+
+async function main() {
+  // Get inputs from action.yml file
+  try {
+    const inputs = {
+      accessKeyId: core.getInput("accessKeyId", { required: true }),
+      secretAccessKey: core.getInput("secretAccessKey", { required: true }),
+      bucket: core.getInput("bucket", { required: true }),
+      localPath: core.getInput("localPath", { required: true }),  // Relative path to files/folder to upload. 
+      remotePath: core.getInput("remotePath", { required: true }),  // Relative path to upoload to. 
+      // clear: getBooleanFromString(core.getInput("clear", { required: false })),  // in case we'll need it in the future
+    };
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+
+  const s3 = new EasyYandexS3({
+    auth: {
+        accessKeyId: inputs.accessKeyId,
+        secretAccessKey: inputs.secretAccessKey,
+    },
+    Bucket: inputs.bucket, 
+    debug: true // Дебаг в консоли, потом можете удалить в релизе
+  });
+
+    // Относительный путь:
+  const upload = await s3.Upload({
+    path: inputs.localPath,  // относительный путь до папки
+    save_name: true // сохранять оригинальные названия файлов 
+  }, inputs.remotePath);
+  console.log(upload);    // <- массив загруженных файлов
   console.log('It works!');
 }
 
-run();
+main();
+
+
