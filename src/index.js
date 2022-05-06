0bc0bda3f9f8f9ed91e9463f2c4ff0825494b922
paulvstrashnov <paulvstrashnov@mail.ru>
2022-05-06 18:01:47 +0300

    get rid of try statement

--- src/index.js
+++ src/index.js
@@ -3,37 +3,31 @@ const EasyYandexS3 = require("easy-yandex-s3"); // https://github.com/powerdot/e
 
 async function main() {
   // Get inputs from action.yml file
-  try {
-    const inputs = {
-      accessKeyId: core.getInput("accessKeyId", { required: true }),
-      secretAccessKey: core.getInput("secretAccessKey", { required: true }),
-      bucket: core.getInput("bucket", { required: true }),
-      localPath: core.getInput("localPath", { required: true }),  // Relative path to files/folder to upload. 
-      remotePath: core.getInput("remotePath", { required: true }),  // Relative path to upoload to. 
-      // clear: getBooleanFromString(core.getInput("clear", { required: false })),  // in case we'll need it in the future
-    };
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+
+  const inputs = {
+    accessKeyId: core.getInput("accessKeyId", { required: true }),
+    secretAccessKey: core.getInput("secretAccessKey", { required: true }),
+    bucket: core.getInput("bucket", { required: true }),
+    localPath: core.getInput("localPath", { required: true }),  // Relative path to files/folder to upload. 
+    remotePath: core.getInput("remotePath", { required: true }),  // Relative path to upoload to. 
+    // clear: getBooleanFromString(core.getInput("clear", { required: false })),  // in case we'll need it in the future
+  };
+
 
   const s3 = new EasyYandexS3({
     auth: {
-        // accessKeyId: inputs.accessKeyId,
-        accessKeyId: core.getInput("accessKeyId", { required: true }),
-        // secretAccessKey: inputs.secretAccessKey,
-        secretAccessKey: core.getInput("secretAccessKey", { required: true }),
+        accessKeyId: inputs.accessKeyId,
+        secretAccessKey: inputs.secretAccessKey,
     },
-    // Bucket: inputs.bucket,
-    Bucket: core.getInput("bucket", { required: true }), 
-    debug: true // Дебаг в консоли, потом можете удалить в релизе
+    Bucket: inputs.bucket,
+    debug: false // Дебаг в консоли, потом можете удалить в релизе
   });
 
     // Относительный путь:
   const upload = await s3.Upload({
-    // path: inputs.localPath,  // относительный путь до папки
-    localPath: core.getInput("localPath", { required: true }),
+    path: inputs.localPath,  // относительный путь до папки
     save_name: true // сохранять оригинальные названия файлов 
-  }, core.getInput("remotePath", { required: true })); // inputs.remotePath);
+  }, inputs.remotePath);
   console.log(upload);    // <- массив загруженных файлов
   // console.log('It works!');
 }
